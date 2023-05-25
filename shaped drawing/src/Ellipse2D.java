7c43f1f0a8a4c6fe580fab47b23592e68b570dc3
Peshala46 <105353970+Peshala46@users.noreply.github.com>
2023-05-25 17:25:29 +0530

    add ellipse 2d visualise design

---
+++ shaped drawing/src/Ellipse2D.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ellipse2D extends JFrame{
+    private JTextField widthTextField;
+    private JTextField xTextField;
+    private JTextField heightTextField;
+    private JTextField yTextField;
+    private JRadioButton greenRadioButton;
+    private JRadioButton blueRadioButton;
+    private JRadioButton redRadioButton;
+    private JButton createButton;
+    private JPanel mypanel4;
+    private JButton backButton;
+    private ButtonGroup colorButtonGroup;
+
+    public Ellipse2D(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(mypanel4);
+        this.pack();
+
+        // Create the ButtonGroup and add radio buttons to it
+        colorButtonGroup = new ButtonGroup();
+        colorButtonGroup.add(redRadioButton);
+        colorButtonGroup.add(greenRadioButton);
+        colorButtonGroup.add(blueRadioButton);
+
+        createButton.addActionListener(e -> {
+            int x = Integer.parseInt(xTextField.getText());
+            int y = Integer.parseInt(yTextField.getText());
+            int width = Integer.parseInt(widthTextField.getText());
+            int height = Integer.parseInt(heightTextField.getText());
+            Color color;
+
+            if (redRadioButton.isSelected()) {
+                color = Color.RED;
+            } else if (greenRadioButton.isSelected()) {
+                color = Color.GREEN;
+            } else if (blueRadioButton.isSelected()) {
+                color = Color.BLUE;
+            } else {
+                color = Color.BLACK;
+            }
+
+            EllipseView2D ellipseView = new EllipseView2D(x, y, width, height, color);
+            ellipseView.setVisible(true);
+            ellipseView.setSize(800, 600);
+        });
+
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Ellipse2D.super.dispose();
+                JFrame frame2= new shapes2D("All 2D");
+                frame2.setVisible(true);
+                frame2.setSize(1000, 600);
+            }
+        });
+    }
+
+}
